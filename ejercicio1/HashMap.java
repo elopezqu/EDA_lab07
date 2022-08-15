d62bfebed9ea9f80f561987779ca1fd5852de485
Edson <elopezqu@unsa.edu.pe>
2022-08-14 19:05:56 -0500

    Se agregan las plantillas

---
+++ ejercicio1/HashMap.java
@@ -0,0 +1,22 @@
+public class HashMap {
+
+    public int size(){return -1;}
+
+    public boolean isEmpty(){return true;}
+
+    public boolean containsKey(Object key){return true;}
+
+    public boolean containsValue(Object value){return true;}
+
+    public Integer get(String key){return -1;}
+
+    public Integer put(String key, Integer value){return -1;}
+
+    public Integer remove(Object key){return -1;}
+
+    public void clear(){}
+
+    public int hashCode(){return -1;}
+
+    public String toString(){return "";};
+}
